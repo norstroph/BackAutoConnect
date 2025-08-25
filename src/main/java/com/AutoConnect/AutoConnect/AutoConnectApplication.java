872c3a510caf47e504a8d5427208bead087f088c
dulon alexandre <43493262+norstroph@users.noreply.github.com>
2025-08-25 15:25:53 +0200

    Initial commit

---
+++ src/main/java/com/AutoConnect/AutoConnect/AutoConnectApplication.java
@@ -0,0 +1,13 @@
+package com.AutoConnect.AutoConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AutoConnect/AutoConnect/AutoConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AutoConnect.AutoConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
