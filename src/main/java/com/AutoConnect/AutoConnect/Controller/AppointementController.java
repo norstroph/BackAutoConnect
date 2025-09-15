b3e0bd8357fdf61e55f810e349cfb1aa0d0730ba
Alex <alexismoreau37320@gmail.com>
2025-09-15 11:28:24 +0200

    test

---
+++ src/main/java/com/AutoConnect/AutoConnect/Controller/AppointementController.java
@@ -0,0 +1,29 @@
+package com.AutoConnect.AutoConnect.Controller;
+
+import com.AutoConnect.AutoConnect.DTO.AppointmentRequestDTO;
+import com.AutoConnect.AutoConnect.DTO.UserResponseDTO;
+import com.AutoConnect.AutoConnect.Entity.Appointment;
+import com.AutoConnect.AutoConnect.Service.AppointmentService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/appointements")
+public class AppointementController {
+
+
+    private final AppointmentService appointmentService;
+
+    public AppointementController(AppointmentService appointmentService){
+        this.appointmentService = appointmentService;
+    };
+
+    @PostMapping
+    public ResponseEntity<AppointmentRequestDTO> createAppointemant(@RequestHeader("Authorization") String authHeader, @Valid  @RequestBody AppointmentRequestDTO appointemantRequestDTO){
+    return new  ResponseEntity<>(appointmentService.saveAppointment(appointemantRequestDTO,authHeader), HttpStatus.CREATED);
+    };
+
+
+}
---
+++ src/main/java/com/AutoConnect/AutoConnect/DTO/Geometry.java
@@ -0,0 +1,17 @@
+package com.AutoConnect.AutoConnect.DTO;
+
+import java.util.List;
+
+public class Geometry {
+    private List<Double> coordinates;
+
+    public Geometry() {}
+
+    public List<Double> getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(List<Double> coordinates) {
+        this.coordinates = coordinates;
+    }
+}
